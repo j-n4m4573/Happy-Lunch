a3558715b3212945a8159342722311e011709c27
j-n4m4573 <jmg220p@gmail.com>
2016-01-20 10:29:47 -0800

    Initial Commit

---
+++ Happy Lunch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Happy Lunch
+//
+//  Created by Jamar Gibbs on 1/20/16.
+//  Copyright © 2016 M1ndful M3d1a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
